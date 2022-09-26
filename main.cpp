d351fc865cec8619fb87e039af0a6ac900bf4eed
Wdnaila <77709778+Wdnaila@users.noreply.github.com>
2022-09-26 22:30:33 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@
  * number of geometry stacks and slices can be adjusted
  * using the + and - keys.
  */
-
+//TITIK
 
 #include <windows.h>
-#ifdef __APPLE__
+#ifdef _APPLE_
 #include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
@@ -27,33 +27,42 @@ void draw()
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0,1.0,1.0);
 
-    glLineWidth(2.5);
-    glBegin(GL_LINES);
+    glPointSize(5.0);
+    glBegin(GL_POINTS);
 
-    glColor3f(0.5,1.1,1.1);
-    glVertex3f(0.2,0.5,0.0);
-
-    glVertex3f(0.8,0.5,0.0);
-
+    glColor3f(1.1,1.1,0.5);
+    glVertex3f(0.7,0.5,0.0);
+    glColor3f(1.1,0.0,0.5);
+    glVertex3f(0.4,0.5,0.0);
+    glColor3f(1.1,0.0,1.1);
+    glVertex3f(0.7,0.2,0.0);
+    glColor3f(1.1,0.5,1.1);
+    glVertex3f(0.6,0.5,0.0);
+    glColor3f(0.1,0.1,1.0);
+    glVertex3f(0.7,0.6,0.0);
+    glColor3f(0.5,0.1,0.1);
+    glVertex3f(0.3,0.5,0.0);
 
     glEnd();
     glFlush();
-}
-void Initialize()
-{
 
+}
+
+void Initialize(){
     glClearColor(0.0,0.0,0.0,0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
 }
+
 int main(int iArgc,char**cppArgv){
     glutInit(&iArgc,cppArgv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250,250);
     glutInitWindowPosition(200,200);
-    glutCreateWindow("point");
+    glutCreateWindow("Point");
     Initialize();
     glutDisplayFunc(draw);
     glutMainLoop();
     return 0;
-
 }
